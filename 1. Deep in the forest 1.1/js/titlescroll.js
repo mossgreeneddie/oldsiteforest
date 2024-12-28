6b4348a18040c1def2346062cb976ebe02f98b5f
Eddie <140919138+mossgreeneddie@users.noreply.github.com>
2024-12-28 13:29:46 +0100

    Delete 1. Deep in the forest 1.1 directory

--- 1. Deep in the forest 1.1/js/titlescroll.js
+++
@@ -1,6 +0,0 @@
-var documentTitle = document.title + " ✦ ";
-
-(function titleMarquee() {
-    document.title = documentTitle = documentTitle.substring(1) + documentTitle.substring(0,1);
-    setTimeout(titleMarquee, 200);
-})();
